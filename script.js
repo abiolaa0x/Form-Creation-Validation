a61ab6415031ec64b23eafb86639d3172c96cc2a
JayCodes <www.amujonathan@gmail.com>
2025-12-06 14:17:02 +0100

    fix: made some changes to meet grader requirements.

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", function () {
   const form = document.getElementById("registration-form");
   const feedbackDiv = document.getElementById("form-feedback");
 
-  form.addEventListener("submit", (e) => {
-    e.preventDefault();
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
 
     const username = document.getElementById("username").value.trim();
     const email = document.getElementById("email").value.trim();
@@ -14,17 +14,21 @@ document.addEventListener("DOMContentLoaded", () => {
 
     if (username.length < 3) {
       isValid = false;
-      messages.push("Invalid username! Must be at least 3 characters long.");
+      messages.push(
+        "Invalid username! Your username must be at least 3 characters long.",
+      );
     }
 
-    if (!(email.includes("@") && email.includes("."))) {
+    if (!email.includes("@") || !email.includes(".")) {
       isValid = false;
       messages.push("Invalid email. Enter a valid email address.");
     }
 
     if (password.length < 8) {
       isValid = false;
-      messages.push("Password must be at least 8 characters long.");
+      messages.push(
+        "Your password is short. Your password must be at least 8 characters long. Try again.",
+      );
     }
 
     feedbackDiv.style.display = "block";
